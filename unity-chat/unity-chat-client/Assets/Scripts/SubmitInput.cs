df6b4ba2c032a5ca5ec8b5ba9c752d1e63ad75aa
ccadori <carloscadori.dev@gmail.com>
2020-04-11 15:38:41 -0300

    client base commit

---
+++ unity-chat/unity-chat-client/Assets/Scripts/SubmitInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class StringUnityEvent : UnityEvent<string> { }
+
+public class SubmitInput : MonoBehaviour
+{
+    public StringUnityEvent onSubmit;
+    public InputField inputField;
+
+    public void Submit()
+    {
+        if (inputField.text.Trim() != "")
+        {
+            Debug.Log(inputField.text.Trim());
+            onSubmit?.Invoke(inputField.text.Trim());
+            inputField.text = "";
+        }
+    }
+}
