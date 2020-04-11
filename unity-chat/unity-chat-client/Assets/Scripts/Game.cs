d7b58789f50fc7af27a521ac8f4d585aff33937a
ccadori <carloscadori.dev@gmail.com>
2020-04-11 16:14:28 -0300

    adding menu scene and submitinput

---
+++ unity-chat/unity-chat-client/Assets/Scripts/Game.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public enum Scene
+{
+    Menu = 0,
+    Chat = 1,
+}
+
+public class Game : MonoBehaviour
+{
+    public Sand.Client client;
+    public UnityEvent onConnected;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        client.Connect();
+        client.Emitter.On("connected", OnConnected);
+    }
+
+    private void OnConnected(string data)
+    {
+        onConnected?.Invoke();
+    }
+
+    public void ChangeScene(Scene scene)
+    {
+        SceneManager.LoadScene((int)scene);
+    }
+}
---
+++ unity-chat/unity-chat-client/Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public Game game;
+    public SubmitInput submitInput;
+
+    private void Start()
+    {
+        submitInput.onSubmit.AddListener(SetName);
+    }
+
+    public void SetName(string name)
+    {
+        game.client.Write("nickname", name);
+        game.ChangeScene(Scene.Chat);
+    }
+}
