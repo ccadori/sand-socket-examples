fb25281c6e7247a538c8a9c8009cc39ff8708458
ccadori <carloscadori.dev@gmail.com>
2020-04-11 16:33:50 -0300

    adding server functionalities

--- unity-chat/unity-chat-server/index.js
+++ unity-chat/unity-chat-server/index.js
@@ -2,4 +2,25 @@ const Sand = require('sand-socket');
 
 const server = new Sand();
 server.verboseLog = true;
+
+server.on('connected', (client) => {
+  
+  // When client send its nickname
+  client.on('nickname', nickname => {
+    client.nickname = nickname;
+    server.writeToAll('message', JSON.stringify({
+      nickname: "System",
+      message: `${client.nickname} has entered the chat.`
+    }), client.id);
+  });
+  
+  // When client send a message
+  client.on('message', message => {
+    server.writeToAll('message', JSON.stringify({
+      nickname: client.nickname,
+      message
+    }));
+  });
+});
+
 server.listen();
