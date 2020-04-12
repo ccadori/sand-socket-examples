67d3152df05f27d69599b5f2380b0c3881ecd2a6
ccadori <carloscadori.dev@gmail.com>
2020-04-11 21:24:31 -0300

    adding menu scene and assets

---
+++ unity-movement-sync/unity-movement-sync-client/Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public Game game;
+    public SubmitInput input;
+
+    private void Start()
+    {
+        input.onSubmit.AddListener(SetNickname);
+    }
+
+    public void SetNickname(string nickname)
+    {
+        game.client.Write("nickname", nickname);
+        game.ChangeScene(Scene.Game);
+    }
+}
---
+++ unity-movement-sync/unity-movement-sync-client/Assets/Scripts/SubmitInput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class StringUnityEvent : UnityEvent<string> { }
+
+public class SubmitInput : MonoBehaviour
+{
+    public InputField inputField;
+    public Button button;
+    public StringUnityEvent onSubmit;
+
+    public void Submit()
+    {
+        if (inputField.text.Trim() != "")
+        {
+            onSubmit?.Invoke(inputField.text.Trim());
+            inputField.text = "";
+        }
+    }
+
+    public void SetInteractable(bool interactable)
+    {
+        inputField.interactable = interactable;
+        button.interactable = interactable;
+    }
+}
